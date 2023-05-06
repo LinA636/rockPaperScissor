43880130e0cd0eefe40c2e7114b3c827112ecefb
Lina <117147821+LinA636@users.noreply.github.com>
2023-05-06 19:44:26 +1200

    fix error with changeImage()

--- javaScript.js
+++ javaScript.js
@@ -20,20 +20,20 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function printScore() {
-    const paraScoreText = document.querySelector(".score");
-    paraScoreText.textContent("test");
-
+function changeScoreDisplay() {
+    const scoreDisplayText = document.querySelector(".score-display");
     if (counterPlayerWin > counterCompWin) {
         // player is leading
         // change "score-display"
+        scoreDisplayText.innerHTML(`You are leading:\n${counterPlayerWin}:${counterCompWin}`);
     } else if (counterPlayerWin < counterCompWin) {
         // computer is leading
         // change "score-display"
-        
+        scoreDisplayText.innerHTML(`You are loosing:\n${counterPlayerWin}:${counterCompWin}`);
     } else {
         // its a tie
         // change "score-display"
+        scoreDisplayText.innerHTML(`It's a tie:\n${counterPlayerWin}:${counterCompWin}`);
     }
 
 }
@@ -73,13 +73,16 @@ function createDOMNodes() {
     leftContainer.appendChild(winLooseAnnouncement);
 
     //children of imgOfSelection-container
+// put images in a iv container so they wont change sizes and are the samse    
     const playerSelectionImg = document.createElement("img");
     playerSelectionImg.classList.add("player-selection");
-    playerSelectionImg.src = "./image/paper.png";
+    playerSelectionImg.src = "./images/paper.png";
     imgOfSelectionContainer.appendChild(playerSelectionImg);
     
     const paraVS = document.createTextNode("vs.");
-    imgOfSelectionContainer.appendChild(paraVS);
+    const paraVScontainer = document.createElement("p");
+    paraVScontainer.appendChild(paraVS);
+    imgOfSelectionContainer.appendChild(paraVScontainer);
 
     const compSelectionImg = document.createElement("img");
     compSelectionImg.classList.add("comp-selection");
@@ -92,35 +95,42 @@ function createDOMNodes() {
     changeContainer.appendChild(rightContainer);
 
     const paraTotalScore = document.createTextNode("total Score:");
-    rightContainer.appendChild(paraTotalScore);
+    const paraTotalScoreContainer = document.createElement("p");
+    paraTotalScoreContainer.appendChild(paraTotalScore);
+    rightContainer.appendChild(paraTotalScoreContainer);
 
     const scoreDisplay = document.createElement("p");
     scoreDisplay.classList.add("score-display");
-/*     scoreDisplay.setAttribute("class", "score-display");
- */    rightContainer.appendChild(scoreDisplay);
+    const scoreText = document.createTextNode("0:0");
+    scoreDisplay.appendChild(scoreText);
+    rightContainer.appendChild(scoreDisplay);
 }
 
 function changeImage(selection, selectorKey){
-    const imgage = document.querySelector(".player-image");
+    const image = document.querySelector(selectorKey);
     if(selection == "paper"){
-        imgage.src = "./images/paper.png";
-        imgage.alt = "Hand fomring a paper.";
+        image.src = "./images/paper.png";
+        image.alt = "Hand fomring a paper.";
     } else if (selection == "rock"){
-        imgage.src = "../image/rock.png";
+        image.src = "./images/rock.png";
     } else if (selection == "scissor"){
-        imgage.src = "./images/scissor.png";
+        image.src = "./images/scissor.png";
     } else {
 
     }
 }
 
 function changeWinLooseAnnouncement(winLoose){
+    const announcement = document.querySelector(".win-loose-announcement");
     if(winLoose == "won"){
         // change text in "win-loose-announcement"
+        announcement.innerHTML(`Congrats, you won this round!`);
     }else if(winLoose == "lost"){
         // change text in "win-loose-announcement"
+        announcement.innerHTML(`Sorry, you lost this round.`);
     }else if(winLoose == "tie"){
         // change text in "win-loose-announcement"
+        announcement.innerHTML(`This is a tie.`);
     }else{
         // error handling
     }
@@ -131,9 +141,6 @@ function changeGameStateText(counterClicks){
     gameStateTextParagraph.innerHTML = `${counterClicks} out of 5`;  
 }
 
-function changeScoreDisplay(){
-    // change the text in "score-display"
-}
 
 function handleButtonClick(playerSelection) {
     counterClicks += 1;
@@ -141,8 +148,8 @@ function handleButtonClick(playerSelection) {
         createDOMNodes();
     }
     let computerSelection = getComputerChoice();
-    changeImage(playerSelection, ".player-image");
-    changeImage(computerSelection, ".comp-image")
+    changeImage(playerSelection, ".player-selection");
+    changeImage(computerSelection, ".comp-selection")
     let winLoose = playRound(playerSelection, computerSelection);
     changeWinLooseAnnouncement(winLoose);
     changeGameStateText(counterClicks);
